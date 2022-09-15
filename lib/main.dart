77b1b95d2e2b46d59db913e36f129937804277c8
Eduardo Fadel Picheth <duhpicheth@gmail.com>
2022-09-15 13:30:08 +0200

    SplashScreen
    SplashScreen working correctly, with transiction and duration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:zaq_test/splash.dart';
+import 'package:zaq_test/dashboard/dashboard.dart';
 
 void main() {
-  runApp(MaterialApp(home: SplashView(),
-
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedSplashScreen(
+          backgroundColor: Colors.black,
+          splash: Image.asset('assets/flutterlogo.png'),
+          nextScreen: const DashboardView(),
+          splashTransition: SplashTransition.rotationTransition,
+          duration: 3000),
+    ),
+  );
 }
-
